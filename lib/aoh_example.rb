c10ebf0d64620a6d81246402db8dc842eabb4d24
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-02 23:13:34 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,8 +8,8 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
   array = [
-    {DON_G},
-    {},
+    DON_G,
+    JOELLE_VD,
     {name:  "Pat Monteseian", occupation: "Staff"},
     {name:  "Kate Gompert", occupation: "None"},
     {name:  "Bruce Green", occupation: "Fan of Mildred"}
