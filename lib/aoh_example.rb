1c79092cc1e2bcd57c784d799703ee5bafc5fbc5
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-02 23:10:09 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -12,7 +12,7 @@ def assembled_aoh
   
   array = [
     {name:  "Don Gately", occupation: "Live-in Staff"},
-    {},
+    {name:  "Joelle van Dyne", occupation: "Radio Personality"},
     {},
     {},
     {}
