1767900ff2a4749f52a665ee6918bd4e1e64f38a
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-02 23:14:21 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -17,9 +17,6 @@ def assembled_aoh
 end
 
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # held in the constants
-  
    array = [
     {name:  "Don Gately", occupation: "Live-in Staff"},
     {name:  "Joelle van Dyne", occupation: "Radio Personality"},
@@ -27,7 +24,6 @@ def literal_aoh
     {name:  "Kate Gompert", occupation: "None"},
     {name:  "Bruce Green", occupation: "Fan of Mildred"}
     ]
-  
 end
 
 def aoh_lookup(aoh, row, key)
